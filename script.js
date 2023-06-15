c6e778672e9514a541fea27c8486c574bcb2f36b
EmadNova <im.nova@yahoo.com>
2023-06-15 21:22:19 +0330

    feat/fix: working on dark mode feature

---
+++ script.js
@@ -0,0 +1,9 @@
+const darkLight = document.querySelector(".switch");
+const round = document.querySelector(".switch__round");
+const fake = document.querySelector(".switch__fake");
+
+darkLight.addEventListener("click", () => {
+    round.classList.toggle("switch__round--active");
+    fake.classList.toggle("switch__fake--active")
+})
+
